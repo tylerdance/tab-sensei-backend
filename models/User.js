eb1997068dd6113e7d8d1265757f1e6850926fa7
Lev Michael <lev.choubine@gmail.com>
2020-12-22 19:58:23 -0500

    backend Andrew's schemas implemented - simple seeder file - get route for comments

--- models/User.js
+++ models/User.js
@@ -1,25 +1,57 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// User schema
+const artistSchema = new Schema({
+  artist_id: Number,
+  name: String,
+  nameWithoutThePrefix: String,
+  useThePrefix: Boolean
+})
+
+const songListSchema = new Schema({
+  songsterr_id: Number,
+  title: String,
+  artist: [artistSchema],
+  chordsPresent: Boolean,
+  url: String
+})
+
+const beatsListSchema = new Schema({
+  title: String,
+  description: String,
+  beats_url: String
+})
+
+const commentsSchema = new Schema({
+  songsterr_id: Number,
+  content: String,
+  // timestamp: Timestamp
+})
+
+const profileSchema = new Schema({
+  username: String,
+  primary_inst: String,
+  stage_name: String,
+  comments: [commentsSchema],
+  song_list: [songListSchema],
+  beats_list: [beatsListSchema]
+})
+
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 8,
-    },
-    date: {
-        type: Date,
-        default: Date.now()
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8
+  },
+  userProfile: [profileSchema]
 });
 
-module.exports = User = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
